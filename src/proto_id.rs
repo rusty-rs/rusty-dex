ec6fb7e3ea67f0fff8cb2b7a845c8f856cda183d
Julien Gamba <julien@jgamba.eu>
2023-02-07 22:18:28 +0100

    Fix proto ids

--- src/proto_id.rs
+++ src/proto_id.rs
@@ -1,5 +1,4 @@
 use std::io::{Seek, SeekFrom};
-use std::collections::HashMap;
 
 use crate::dex_reader::DexReader;
 use crate::strings::StringData;
@@ -13,7 +12,7 @@ pub struct ProtoIdItem {
 
 #[derive(Debug)]
 pub struct ProtoIdList {
-    pub items: HashMap<u32, ProtoIdItem>
+    pub items: Vec<ProtoIdItem>
 }
 
 impl ProtoIdList {
@@ -23,24 +22,17 @@ impl ProtoIdList {
                  strings_list: &StringData) -> Self {
         dex_reader.bytes.seek(SeekFrom::Start(offset.into())).unwrap();
 
-        let mut protos = HashMap::new();
+        let mut protos = Vec::new();
 
         for i in 0..size {
-            let proto_offset = dex_reader.read_u32().unwrap();
-            let current_offset = dex_reader.bytes.position();
-
-            dex_reader.bytes.seek(SeekFrom::Start(proto_offset.into())).unwrap();
-
             let shorty_idx = dex_reader.read_u32().unwrap();
             let return_type_idx = dex_reader.read_u32().unwrap();
             let parameters_off = dex_reader.read_u32().unwrap();
-            protos.insert(proto_offset,
-                          ProtoIdItem { shorty_idx,
-                                        return_type_idx,
-                                        parameters_off });
-                          // strings_list.strings[offset as usize].string.clone());
-
-            dex_reader.bytes.seek(SeekFrom::Start(current_offset)).unwrap();
+            protos.push(ProtoIdItem {
+                shorty_idx,
+                return_type_idx,
+                parameters_off
+            });
         }
 
         ProtoIdList { items: protos }
