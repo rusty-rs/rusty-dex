00da3310906a90cae5e8360826d718ce16a44adc
Julien Gamba <julien@jgamba.eu>
2023-02-08 11:54:58 +0100

    Added more constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,26 +1,100 @@
-pub struct MapItemType;
-
 #[allow(non_camel_case_types)]
+
+/* Types of items present in the header map of a DEX file */
+pub struct MapItemType;
 impl MapItemType {
-    pub const HEADER_ITEM: u16 = 0x0000;
-    pub const STRING_ID_ITEM: u16 = 0x0001;
-    pub const TYPE_ID_ITEM: u16 = 0x0002;
-    pub const PROTO_ID_ITEM: u16 = 0x0003;
-    pub const FIELD_ID_ITEM: u16 = 0x0004;
-    pub const METHOD_ID_ITEM: u16 = 0x0005;
-    pub const CLASS_DEF_ITEM: u16 = 0x0006;
-    pub const CALL_SITE_ID_ITEM: u16 = 0x0007;
-    pub const METHOD_HANDLE_ITEM: u16 = 0x0008;
-    pub const MAP_LIST: u16 = 0x1000;
-    pub const TYPE_LIST: u16 = 0x1001;
-    pub const ANNOTATION_SET_REF_LIST: u16 = 0x1002;
-    pub const ANNOTATION_SET_ITEM: u16 = 0x1003;
-    pub const CLASS_DATA_ITEM: u16 = 0x2000;
-    pub const CODE_ITEM: u16 = 0x2001;
-    pub const STRING_DATA_ITEM: u16 = 0x2002;
-    pub const DEBUG_INFO_ITEM: u16 = 0x2003;
-    pub const ANNOTATION_ITEM: u16 = 0x2004;
-    pub const ENCODED_ARRAY_ITEM: u16 = 0x2005;
+    pub const HEADER_ITEM               : u16 = 0x0000;
+    pub const STRING_ID_ITEM            : u16 = 0x0001;
+    pub const TYPE_ID_ITEM              : u16 = 0x0002;
+    pub const PROTO_ID_ITEM             : u16 = 0x0003;
+    pub const FIELD_ID_ITEM             : u16 = 0x0004;
+    pub const METHOD_ID_ITEM            : u16 = 0x0005;
+    pub const CLASS_DEF_ITEM            : u16 = 0x0006;
+    pub const CALL_SITE_ID_ITEM         : u16 = 0x0007;
+    pub const METHOD_HANDLE_ITEM        : u16 = 0x0008;
+    pub const MAP_LIST                  : u16 = 0x1000;
+    pub const TYPE_LIST                 : u16 = 0x1001;
+    pub const ANNOTATION_SET_REF_LIST   : u16 = 0x1002;
+    pub const ANNOTATION_SET_ITEM       : u16 = 0x1003;
+    pub const CLASS_DATA_ITEM           : u16 = 0x2000;
+    pub const CODE_ITEM                 : u16 = 0x2001;
+    pub const STRING_DATA_ITEM          : u16 = 0x2002;
+    pub const DEBUG_INFO_ITEM           : u16 = 0x2003;
+    pub const ANNOTATION_ITEM           : u16 = 0x2004;
+    pub const ENCODED_ARRAY_ITEM        : u16 = 0x2005;
     pub const ANNOTATIONS_DIRECTORY_ITEM: u16 = 0x2006;
-    pub const HIDDENAPI_CLASS_DATA_ITEM: u16 = 0xF000;
+    pub const HIDDENAPI_CLASS_DATA_ITEM : u16 = 0xF000;
+}
+
+/* Method types */
+pub struct MethodHandleType;
+impl MethodHandleType {
+    /* Method handle is a static field setter (accessor) */
+    pub const METHOD_HANDLE_TYPE_STATIC_PUT        : u16 = 0x00;
+    /* Method handle is a static field getter (accessor) */
+    pub const METHOD_HANDLE_TYPE_STATIC_GET        : u16 = 0x01;
+    /* Method handle is an instance field setter (accessor) */
+    pub const METHOD_HANDLE_TYPE_INSTANCE_PUT      : u16 = 0x02;
+    /* Method handle is an instance field getter (accessor) */
+    pub const METHOD_HANDLE_TYPE_INSTANCE_GET      : u16 = 0x03;
+    /* Method handle is a static method invoker */
+    pub const METHOD_HANDLE_TYPE_INVOKE_STATIC     : u16 = 0x04;
+    /* Method handle is an instance method invoker */
+    pub const METHOD_HANDLE_TYPE_INVOKE_INSTANCE   : u16 = 0x05;
+    /* Method handle is a constructor method invoker */
+    pub const METHOD_HANDLE_TYPE_INVOKE_CONSTRUCTOR: u16 = 0x06;
+    /* Method handle is a direct method invoker */
+    pub const METHOD_HANDLE_TYPE_INVOKE_DIRECT     : u16 = 0x07;
+    /* Method handle is an interface method invoker */
+    pub const METHOD_HANDLE_TYPE_INVOKE_INTERFACE  : u16 = 0x08;
+}
+
+/* Bitfields of these flags are used to indicate the accessibility
+ * and overall properties of classes and class members. */
+pub struct AccessFlags;
+impl AccessFlags {
+    pub const ACC_PUBLIC               : u32 = 0x1;
+    pub const ACC_PRIVATE              : u32 = 0x2;
+    pub const ACC_PROTECTED            : u32 = 0x4;
+    pub const ACC_STATIC               : u32 = 0x8;
+    pub const ACC_FINAL                : u32 = 0x10;
+    pub const ACC_SYNCHRONIZED         : u32 = 0x20;
+    pub const ACC_VOLATILE             : u32 = 0x40;
+    pub const ACC_BRIDGE               : u32 = 0x40;
+    pub const ACC_TRANSIENT            : u32 = 0x80;
+    pub const ACC_VARARGS              : u32 = 0x80;
+    pub const ACC_NATIVE               : u32 = 0x100;
+    pub const ACC_INTERFACE            : u32 = 0x200;
+    pub const ACC_ABSTRACT             : u32 = 0x400;
+    pub const ACC_STRICT               : u32 = 0x800;
+    pub const ACC_SYNTHETIC            : u32 = 0x1000;
+    pub const ACC_ANNOTATION           : u32 = 0x2000;
+    pub const ACC_ENUM                 : u32 = 0x4000;
+    pub const ACC_CONSTRUCTOR          : u32 = 0x10000;
+    pub const ACC_DECLARED_SYNCHRONIZED: u32 = 0x20000;
+}
+
+/* An encoded value is an encoded piece of (nearly) arbitrary
+ * hierarchically structured data. The encoding is meant to be
+ * both compact and straightforward to parse. */
+pub struct EncodedValueFormat;
+impl EncodedValueFormat {
+    pub const VALUE_BYTE         : u8 = 0x00;
+    pub const VALUE_SHORT        : u8 = 0x02;
+    pub const VALUE_CHAR         : u8 = 0x03;
+    pub const VALUE_INT          : u8 = 0x04;
+    pub const VALUE_LONG         : u8 = 0x06;
+    pub const VALUE_FLOAT        : u8 = 0x10;
+    pub const VALUE_DOUBLE       : u8 = 0x11;
+    pub const VALUE_METHOD_TYPE  : u8 = 0x15;
+    pub const VALUE_METHOD_HANDLE: u8 = 0x16;
+    pub const VALUE_STRING       : u8 = 0x17;
+    pub const VALUE_TYPE         : u8 = 0x18;
+    pub const VALUE_FIELD        : u8 = 0x19;
+    pub const VALUE_METHOD       : u8 = 0x1a;
+    pub const VALUE_ENUM         : u8 = 0x1b;
+    pub const VALUE_ARRAY        : u8 = 0x1c;
+    pub const VALUE_ANNOTATION   : u8 = 0x1d;
+    pub const VALUE_NULL         : u8 = 0x1e;
+    pub const VALUE_BOOLEAN      : u8 = 0x1f;
 }
