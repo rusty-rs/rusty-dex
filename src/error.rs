28b6935b70619d0391da7dc93e7b879c6c5c8111
Julien Gamba <julien@jgamba.eu>
2023-03-23 23:36:34 +0100

    Unit tests in error.rs

--- src/error.rs
+++ src/error.rs
@@ -24,3 +24,25 @@ impl Error for DexError {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let error = DexError::new("test message");
+        assert_eq!(error.message, "test message");
+    }
+
+    #[test]
+    fn test_display() {
+        let error = DexError::new("test message");
+        assert_eq!(error.to_string(), "test message");
+    }
+
+    #[test]
+    fn test_description() {
+        let error = DexError::new("test message");
+        assert_eq!(error.description(), "test message");
+    }
+}
