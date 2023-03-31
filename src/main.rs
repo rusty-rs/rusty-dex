45fe780ceb3d5f11904ef1f6c06a832548d6d952
Julien Gamba <julien@jgamba.eu>
2023-03-31 15:09:47 +0200

    Create DexReader object from APK path directly

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-use std::fs::File;
-use std::io::Read;
-use zip::ZipArchive;
 use clap::Parser;
 
 extern crate dex_parser;
 
 use dex_parser::logging;
-use dex_parser::{info, die};
+use dex_parser::{ info };
 
 use dex_parser::dex_reader::DexReader;
 use dex_parser::dex_file::DexFile;
@@ -17,28 +14,9 @@ fn main() {
     info!("Set log level to {}", cli_args.log_level);
 
     info!("Loading classes.dex from {}", cli_args.apk);
-    let raw_file = File::open(cli_args.apk)
-        .unwrap_or_else(|err| die!("Could not open input file: {err}"));
-    let mut zip_file = ZipArchive::new(raw_file)
-        .unwrap_or_else(|err| die!("Error: cannot create ZipArchive object: {err}"));
-
-    /* TODO: support merging of multiple DEX files
-     * I dug around a little and it seems like this should be
-     * pretty straightforward. Each classes.dex file in apps apps
-     * with multiple DEX files are basically all valid and can be
-     * parsed the same way. Then we can merge all the data into
-     * one `DexFile` struct by eliminating duplicates and
-     * re-sorting the list of values.
-     **/
-    let mut dex_entry = zip_file.by_name("classes.dex")
-                                .unwrap_or_else(|_| die!("Error: cannot find classes.dex in the APK"));
-
-    let mut raw_dex = Vec::new();
-    dex_entry.read_to_end(&mut raw_dex)
-             .unwrap_or_else(|err| die!("Could not read input file: {err}"));
+    let dex_reader = DexReader::build(&cli_args.apk);
 
     info!("Parsing DEX file");
-    let dex_reader = DexReader::build(raw_dex);
     let dex_file = DexFile::build(dex_reader);
     dex_file.disasm();
 }
