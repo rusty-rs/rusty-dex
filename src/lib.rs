9d9d84e487284305c5ec9f29118059d10b7b8eb9
Julien Gamba <julien@jgamba.eu>
2024-05-20 16:00:43 +0000

    Added get_qualified_method_names method

--- src/lib.rs
+++ src/lib.rs
@@ -43,3 +43,19 @@ pub fn parse(filepath: &str) -> DexFile {
     let readers = DexReader::build_from_file(filepath);
     DexFile::merge(readers)
 }
+
+pub fn get_qualified_method_names(dex: &DexFile) -> Vec<String> {
+    let mut methods = Vec::new();
+
+    let class_names = dex.get_classes_names();
+    for class in class_names.iter() {
+        if let Some(class_def) = dex.classes.get_class_def(class) {
+            for method in class_def.get_methods() {
+                let name = method.get_method_name();
+                methods.push(format!("{class}->{name}"));
+            }
+        }
+    }
+
+    methods
+}
